16b803fdf5c7b8a1454fb241ef97811352019886
zbari023 <barry450643@gmail.com>
2023-11-12 00:14:28 +0100

    try to connect vue with th-loop

--- todo/models.py
+++ todo/models.py
@@ -2,17 +2,12 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
-STATUS = (
-    ('notstarted', 'notstarted'),
-    ('inprogress', 'inprogress'),
-    ('completed', 'completed'),
-)
+
 
 class Todo(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField(max_length=200)
-    status = models.CharField(max_length=20,choices=STATUS)
-    created_datum = models.DateTimeField(default=timezone.now)
+    todo = models.TextField(max_length=2000)
+    Notes= models.TextField(max_length=2000)
+    Action = models.TextField(max_length=2000)
     def __str__(self):
-        return self.name
+        return self.todo
     
